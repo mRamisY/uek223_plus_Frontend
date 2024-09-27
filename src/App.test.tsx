8a7599840a05fb689f74c7e1fe000c3db65611be
MuhammadRamisYousaf <142780542+MuhammadRamisYousaf@users.noreply.github.com>
2024-09-27 09:09:26 +0200

    Refactor App.test.tsx to update test description

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,12 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome message', () => {
   render(
       <MemoryRouter>
         <App />
       </MemoryRouter>
   );
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeElement = screen.getByText(/Welcome to the Homepage/i);
+  expect(welcomeElement).toBeInTheDocument();
 });
